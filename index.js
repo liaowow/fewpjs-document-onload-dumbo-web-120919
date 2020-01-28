a716df694b7c2e366ebccbf5dce86d6d5e35b6c8
Annie Liao <liaowow@gmail.com>
2020-01-28 12:15:33 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    replaceText();
+});
+
+function replaceText() {
+    document.querySelector("#text").textContent = 'This is really cool!';
+}
